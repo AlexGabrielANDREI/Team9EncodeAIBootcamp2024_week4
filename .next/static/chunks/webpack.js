fcf4d2bfece81e8228bfc3bc5e3c0a79aae4ec3e
RidwanOseni <ridwanoseni101@gmail.com>
2024-11-25 09:08:58 -0300

    Added story-telling app with modifications to integrate character API

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "e77bec87d3adddc1"; }
+/******/ 		__webpack_require__.h = function() { return "ab8d9ed23279172d"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
